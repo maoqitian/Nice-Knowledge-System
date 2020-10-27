0f6fba05515d52beeeb78a2e7b64c8a93097d2b3
maoqitian <maoqitian068@163.com>
2020-10-28 00:07:47 +0800

    add java review demo

---
+++ 计算机基础/JavaReview/src/main/java/clazz/ClazzTest.java
@@ -0,0 +1,18 @@
+package clazz;
+
+
+/**
+ * @Description: class 字节码解析测试
+ * @Author: maoqitian
+ * @CreateDate: 2020/10/21 22:47
+ */
+public class ClazzTest {
+
+    private int a = 0;
+
+    public int add(int x,int y){
+        return  x+y;
+    }
+
+
+}
---
+++ 计算机基础/JavaReview/src/main/java/volatileTest/VolatileClass.java
@@ -0,0 +1,54 @@
+package volatileTest;
+
+/**
+ * @Description:
+ * @Author: maoqitian
+ * @CreateDate: 2020/10/27 20:01
+ */
+public class VolatileClass {
+
+
+    public static void main(String[] args) {
+
+        TestThread testThread = new TestThread();
+         testThread.start();
+
+         //主线程
+         //System.out.println("currentThread()"+ Thread.currentThread().getName());
+         while (true){
+            synchronized (testThread){
+                if(testThread.isFlag()){
+                    System.out.println("wdnmd .....");
+
+                }
+            }
+
+        }
+
+    }
+
+    static class TestThread extends Thread{
+
+        public boolean isFlag() {
+            return flag;
+        }
+
+        private  boolean flag = false;
+        //private volatile boolean flag = false;
+
+
+        @Override
+        public void run() {
+            super.run();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            flag = true;
+            System.out.println("flag : "+ flag+" threadid " + currentThread().getName());
+        }
+    }
+}
